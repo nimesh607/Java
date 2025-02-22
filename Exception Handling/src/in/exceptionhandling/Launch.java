c93780afc2134c1d1dfe7564fbcc4c2606f30d2e
nimesh607 <48921126+nimesh607@users.noreply.github.com>
2025-02-22 18:32:21 +0530

    Add files via upload

---
+++ Exception Handling/src/in/exceptionhandling/Launch.java
@@ -0,0 +1,15 @@
+package in.exceptionhandling;
+
+public class Launch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c= 100/0;
+		System.out.println("hello");
+		
+		//throw new ArithmeticException("/by zero");
+		//System.out.println("nor");
+
+	}
+
+}
